202a0b44bccac94b22a0f6932b7ad095db2a2b8e
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-09-22 11:58:35 +0200

    Game scanning code

--- MPXInstaller/Form1.cs
+++ MPXInstaller/Form1.cs
@@ -1,23 +1,35 @@
 using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace MPXInstaller
 {
     public partial class frmMain : Form
     {
         private string steamPath = "";
-        private static readonly string configFileUrl = "https://"
+        private static readonly string configFileUrl = "https://raw.githubusercontent.com/mindphluxnet/MPXInstaller/master/MPXInstaller/config/GameConfig.json";
+        private List<GameConfig> gameConfigs = new List<GameConfig>();
 
         public frmMain()
         {
             InitializeComponent();
         }
 
-        private void frmMain_Shown(object sender, EventArgs e)
+        private async void frmMain_Shown(object sender, EventArgs e)
         {
             string? _steamPath = GetSteamPath();
             if (_steamPath != null && Directory.Exists(_steamPath))
             {
                 steamPath = _steamPath;
+                await FetchSupportedGames();
+                lbInstalledGames.Items.Clear();
+                foreach(GameConfig _gc in gameConfigs)
+                {
+                    if(Directory.Exists(Path.Combine(steamPath, _gc.InstallPath)))
+                    {
+                        lbInstalledGames.Items.Add(_gc.Name);
+                    }
+                }
             }
             else
             {
@@ -48,9 +60,30 @@ namespace MPXInstaller
             return null;
         }
 
-        private void FetchSupportedGames()
+        private async Task FetchSupportedGames()
         {
-
+            try
+            {
+                using(HttpClient client = new())
+                {
+                    HttpResponseMessage response = await client.GetAsync(configFileUrl);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonContent = await response.Content.ReadAsStringAsync();
+                        gameConfigs = JsonConvert.DeserializeObject<List<GameConfig>>(jsonContent);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Unable to download configuration file. Please try again later!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unable to download configuration file. Please try again later!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
         }
     }
 }
